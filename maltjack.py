a611cdb343c41c6a17d88da111c51bbf6bb762b3
Huw Wilkins <huwshimi@gmail.com>
2014-02-21 21:05:33 +1100

    Added scss building.

--- maltjack.py
+++ maltjack.py
@@ -1,4 +1,5 @@
 import argparse
+import scss
 import os
 import shutil
 import SimpleHTTPServer
@@ -11,17 +12,21 @@ ARGS = None
 PORT = 8000
 BUILD_DIR = 'build'
 CONTENT_DIR = 'content'
+MEDIA_DIR = 'media'
 
 class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 
     def translate_path(self, path):
         path = path.strip('/')
-        file_path = os.path.join(ARGS.directory, BUILD_DIR)
-        filename =  os.path.join(file_path, '%s.html' % path)
-        if os.path.isfile(filename):
-            return filename
+        if path.split('/')[0] != MEDIA_DIR:
+            file_path = os.path.join(ARGS.directory, BUILD_DIR)
+            filename =  os.path.join(file_path, '%s.html' % path)
+            if os.path.isfile(filename):
+                return filename
+            else:
+                return os.path.join(file_path, path, 'index.html')
         else:
-            return os.path.join(file_path, path, 'index.html')
+            return os.path.join(ARGS.directory, BUILD_DIR, path)
 
     def do_GET(self):
         build_site(ARGS.directory)
@@ -29,12 +34,16 @@ class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
         SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
 
 def build_site(project_dir):
-    env = Environment(loader=FileSystemLoader(project_dir))
-    content_dir = os.path.join(project_dir, 'content')
     build_dir = os.path.join(project_dir, BUILD_DIR)
     if os.path.exists(build_dir):
         shutil.rmtree(build_dir)
     os.makedirs(build_dir)
+    build_pages(project_dir, build_dir)
+    build_media(project_dir, build_dir)
+
+def build_pages(project_dir, build_dir):
+    env = Environment(loader=FileSystemLoader(project_dir))
+    content_dir = os.path.join(project_dir, 'content')
 
     for root, dirs, filenames in os.walk(content_dir):
         for f in filenames:
@@ -47,6 +56,33 @@ def build_site(project_dir):
             rendered = template.render()
             with open(build_path, 'w') as f:
                 f.write(rendered)
+
+def build_scss(media_build):
+    css_dir = os.path.join(media_build, 'css')
+    scss.config.LOAD_PATHS = [css_dir]
+    css = scss.Scss()
+
+    for root, dirs, filenames in os.walk(css_dir):
+        for f in filenames:
+            if f.rsplit('.', 1)[1] == 'scss' and f[:1] != '_':
+                filename = os.path.join(root, f)
+                new_file = '%s.css' % filename.rsplit('.', 1)[0]
+                os.chdir(root)
+                with open(filename, 'r') as f:
+                    compiled = css.compile(f.read())
+                with open(new_file, 'w') as f:
+                    f.write(compiled)
+
+    for root, dirs, filenames in os.walk(css_dir):
+        for f in filenames:
+            if f.rsplit('.', 1)[1] == 'scss':
+                os.remove(os.path.join(root, f))
+
+def build_media(project_dir, build_dir):
+    media_dir = os.path.join(project_dir, 'template', MEDIA_DIR)
+    media_build = os.path.join(build_dir, MEDIA_DIR)
+    shutil.copytree(media_dir, media_build)
+    build_scss(media_build)
 
 def run_server(project_dir):
     os.chdir(os.path.join(project_dir, BUILD_DIR))
