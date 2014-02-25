7dadb9594754cee23619f7126326bed60debc5eb
Huw Wilkins <huwshimi@gmail.com>
2014-02-25 11:11:05 +1100

    Added support for content images.

--- maltjack.py
+++ maltjack.py
@@ -15,12 +15,14 @@ PORT = 8000
 BUILD_DIR = 'build'
 CONTENT_DIR = 'content'
 MEDIA_DIR = 'media'
+IMAGE_DIR = 'images'
 
 class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 
     def translate_path(self, path):
         path = path.strip('/')
-        if path.split('/')[0] != MEDIA_DIR:
+        path_start = path.split('/')[0]
+        if path_start != MEDIA_DIR and path_start != IMAGE_DIR:
             file_path = os.path.join(ARGS.directory, BUILD_DIR)
             filename =  os.path.join(file_path, '%s.html' % path)
             if os.path.isfile(filename):
@@ -48,16 +50,17 @@ def build_pages(project_dir, build_dir):
     content_dir = os.path.join(project_dir, 'content')
 
     for root, dirs, filenames in os.walk(content_dir):
-        for f in filenames:
-            filename = os.path.join(root, f)
-            file_relative = filename.replace(content_dir + '/', '')
-            build_path = os.path.join(build_dir, file_relative)
-            if not os.path.exists(os.path.dirname(build_path)):
-                os.makedirs(os.path.dirname(build_path))
-            template = env.get_template('%s/%s' % (CONTENT_DIR, file_relative))
-            rendered = template.render()
-            with open(build_path, 'w') as f:
-                f.write(rendered)
+        if root.rsplit('/', 1)[1] != IMAGE_DIR:
+            for f in filenames:
+                filename = os.path.join(root, f)
+                file_relative = filename.replace(content_dir + '/', '')
+                build_path = os.path.join(build_dir, file_relative)
+                if not os.path.exists(os.path.dirname(build_path)):
+                    os.makedirs(os.path.dirname(build_path))
+                template = env.get_template('%s/%s' % (CONTENT_DIR, file_relative))
+                rendered = template.render()
+                with open(build_path, 'w') as f:
+                    f.write(rendered)
 
 def build_scss(media_build):
     css_dir = os.path.join(media_build, 'css')
